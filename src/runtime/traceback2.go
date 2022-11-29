f4127cc041d12fd486cdba2e1ac81ca5c2df6d7e
Felix Geisendörfer <felix.geisendoerfer@datadoghq.com>
2022-11-29 10:26:11 +0100

    refactor: tracebackIterator add input fields
    Change-Id: I6a61f948d6b930d0376c19af8387c8d8d3626d12

--- src/runtime/traceback2.go
+++ src/runtime/traceback2.go
@@ -11,11 +11,19 @@ import (
 )
 
 func gentraceback2(pc0, sp0, lr0 uintptr, gp *g, skip int, pcbuf *uintptr, max int, callback func(*stkframe, unsafe.Pointer) bool, v unsafe.Pointer, flags uint) int {
-	var itr tracebackIterator
+	itr := tracebackIterator{}
 	return itr.Gentraceback(pc0, sp0, lr0, gp, skip, pcbuf, max, callback, v, flags)
 }
 
 type tracebackIterator struct {
+	pc0, sp0, lr0 uintptr
+	gp            *g
+	skip          int
+	pcbuf         *uintptr
+	max           int
+	callback      func(*stkframe, unsafe.Pointer) bool
+	v             unsafe.Pointer
+	flags         uint
 }
 
 func (itr *tracebackIterator) Gentraceback(pc0, sp0, lr0 uintptr, gp *g, skip int, pcbuf *uintptr, max int, callback func(*stkframe, unsafe.Pointer) bool, v unsafe.Pointer, flags uint) int {
